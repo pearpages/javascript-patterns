d5a95a40a1a334c75dfbf6d24338835b2662d06e
pearpages <pere@soms.cat>
2016-04-01 11:05:20 +0200

    javascript prototypes

--- patterns/prototypes/index.js
+++ patterns/prototypes/index.js
@@ -3,15 +3,15 @@
 var Task = function (name) {
 	this.name = name;
 	this.completed = false;
+};
 
-	this.complete = function () {
-		console.log('completing task: '+this.name);
-		this.completed = true;
-	};
+Task.prototype.complete = function () {
+	console.log('completing task: '+this.name);
+	this.completed = true;
+};
 
-	this.save = function () {
-		console.log('saving Task: ' + this.name);
-	};
+Task.prototype.save = function () {
+	console.log('saving Task: ' + this.name);
 };
 
 
