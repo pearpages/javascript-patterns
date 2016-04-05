2eb88160c63d7748dd4b4357b796aa1c858ce277
pearpages <pere@soms.cat>
2016-04-05 18:48:59 +0200

    mediator pattern

--- patterns/mediator/main.js
+++ patterns/mediator/main.js
@@ -2,22 +2,33 @@ var mediator = require('./mediator');
 
 var Task = require('./task');
 
+// overwritting original complete function
+Task.prototype.complete = function (){
+	console.log('Completed task '+this.name);
+	mediator.publish('complete', this);
+}
+
+var Not = new require('./observers/notificationService');
+var Ls = new require('./observers/loggingService');
+var Audit = new require('./observers/auditingService');
+
 var task1 = new Task({
 	name: 'demo task',
 	user: 'John Doe'
 });
 
-task1.complete = function () {
-	mediator.publish('complete', this);
-	Task.prototype.complete.call(this);
-};
+var task2 = new Task({
+	name: 'buy toilet papper',
+	user: 'Pere Pages'
+});
 
-task1.complete();
-
-var not = new require('./observers/notificationService');
-var ls = new require('./observers/loggingService');
-var audit = new require('./observers/auditingService');
+var not = new Not();
+var ls = new Ls();
+var audit = new Audit();
 
 mediator.subscribe('complete', not, not.update); // channel, context, function
 mediator.subscribe('complete', ls, ls.update); 
 mediator.subscribe('complete', audit, audit.update); 
+
+task1.complete();
+task2.complete();
