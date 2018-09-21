10eed6aa1b50986409f279c2bed3619555a492a6
pearpages <pere@soms.cat>
2018-09-21 21:31:27 +0200

    feat: observer pattern

--- src/observer/weather-station.ts
+++ src/observer/weather-station.ts
@@ -8,10 +8,20 @@ interface Observer {
     update(temp: number, humidity: number, pressure: number): void;
 }
 
-abstract class DisplayElement {
+abstract class ConcreteDisplayObserver implements Observer, Display {
+    constructor(subject: Subject) {
+        subject.registerObserver(this);
+    }
+    update(temp: number, humidity: number, pressure: number): void {
+        this.display(`${this.constructor.name} temp: ${temp}, humidity: ${humidity}, pressure: ${pressure}`);
+    }
     display(data: string): void {
         console.log(data);
     }
+}
+
+interface Display {
+    display(data: string): void;
 }
 
 class WeatherData implements Subject {
@@ -60,42 +70,22 @@ class WeatherData implements Subject {
     }
 }
 
-class CurrentConditionsDisplay extends DisplayElement implements Observer {
-    update(temp: number, humidity: number, pressure: number): void {
-        this.display(`CurrentConditionsDisplay temp: ${temp}, humidity: ${humidity}, pressure: ${pressure}`);
-    }
-}
+class CurrentConditionsDisplay extends ConcreteDisplayObserver {}
 
-class StatisticsDisplay extends DisplayElement implements Observer {
-    update(temp: number, humidity: number, pressure: number): void {
-        this.display(`StatisticsDisplay temp: ${temp}, humidity: ${humidity}, pressure: ${pressure}`);
-    }
-}
+class StatisticsDisplay extends ConcreteDisplayObserver {}
 
-class ThirdPartyDisplay extends DisplayElement implements Observer {
-    update(temp: number, humidity: number, pressure: number): void {
-        this.display(`ThirdPartyDisplay temp: ${temp}, humidity: ${humidity}, pressure: ${pressure}`);
-    }
-}
+class ThirdPartyDisplay extends ConcreteDisplayObserver {}
 
-class ForecastDisplay extends DisplayElement implements Observer {
-    update(temp: number, humidity: number, pressure: number): void {
-        this.display(`ForecastDisplay temp: ${temp}, humidity: ${humidity}, pressure: ${pressure}`);
-    }
-}
+class ForecastDisplay extends ConcreteDisplayObserver {}
 
 export class ObserverExample {
     private constructor() {}
     static run(): void {
         const weatherData = new WeatherData();
-        const currentConditionsDisplay = new CurrentConditionsDisplay();
-        const statisticsDisplay = new StatisticsDisplay();
-        const thirdPartyDisplay = new ThirdPartyDisplay();
-        const forecastDisplay = new ForecastDisplay();
-        weatherData.registerObserver(currentConditionsDisplay);
-        weatherData.registerObserver(statisticsDisplay);
-        weatherData.registerObserver(thirdPartyDisplay);
-        weatherData.registerObserver(forecastDisplay);
+        const currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        const statisticsDisplay = new StatisticsDisplay(weatherData);
+        const thirdPartyDisplay = new ThirdPartyDisplay(weatherData);
+        const forecastDisplay = new ForecastDisplay(weatherData);
         weatherData.setMeasuraments(makeNumber(), makeNumber(), makeNumber());
         weatherData.setMeasuraments(makeNumber(), makeNumber(), makeNumber());
         weatherData.setMeasuraments(makeNumber(), makeNumber(), makeNumber());
